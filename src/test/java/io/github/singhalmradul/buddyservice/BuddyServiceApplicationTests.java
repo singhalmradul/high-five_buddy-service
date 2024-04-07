d5fd8bee97f904f5885f0148d0d886a655b7e592
Mradul Singhal <singhalmradul@gmail.com>
2024-04-07 07:02:41 -0400

    initialize buddy-service

---
+++ src/test/java/io/github/singhalmradul/buddyservice/BuddyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.singhalmradul.buddyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
